e4ff17aa0e93c12633c1e66f3c99eb9f5bfef66e
unknown <indianic@ind496.inic.local>
2018-03-06 11:06:46 +0530

    This is Mavan Java Project

---
+++ src/test/java/Selenium/AppiumTest.java
@@ -0,0 +1,20 @@
+package Selenium;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void test3(){
+		
+		System.out.println("Test 3");
+	}
+	
+	@Test
+	public void test4(){
+		
+		System.out.println("Test 4");
+	}
+
+
+}
---
+++ src/test/java/Selenium/RestAPITest.java
@@ -0,0 +1,20 @@
+package Selenium;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void test5(){
+		
+		System.out.println("Test 5");
+	}
+	
+	@Test
+	public void test6(){
+		
+		System.out.println("Test 6");
+	}
+
+
+}
